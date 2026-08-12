e1adc3256211204f1b8411bf7258308ebe519519
Rafael <camarodorato@gmail.com>
2026-08-12 20:04:57 -0300

    chore: limpa projeto inicial

---
+++ App.tsx
@@ -0,0 +1,19 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.tsx to start working on your app!</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#e40f0f",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+});
